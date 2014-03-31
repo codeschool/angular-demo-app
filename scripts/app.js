c2dcc7694b8a219e6ec0c57bb786ef68fc54f1ca
David Rogers <david@orlandophp.org>
2014-03-31 17:19:11 -0400

    Defining our base `<product-gallery>` Directive and changing the `GalleryController` API a tad...
    @alyssamichelle and I discussed using indexes instead of objects for the value of `GalleryController.current`, and I hope this was the correct implementation... Please confirm?

--- scripts/app.js
+++ scripts/app.js
@@ -3,13 +3,20 @@
     .controller("StoreController", function(){
       this.products = [
         { name: 'Some Awesome Product', price: 123.45,
-          images: [ ],
+          images: [
+            { full: 'product01-image01-full.jpg', thumb: 'product01-image01-thumb.jpg' },
+            { full: 'product01-image02-full.jpg', thumb: 'product01-image02-thumb.jpg' },
+            { full: 'product01-image03-full.jpg', thumb: 'product01-image03-thumb.jpg' },
+          ],
           description: "This product is awesome!",
           specifications: "Filled with Awesome",
           reviews: [ ]
         },
         { name: 'Some Other Awesome Product', price: 123.45,
-          images: [ ],
+          images: [
+            { full: 'product02-image01-full.jpg', thumb: 'product02-image01-thumb.jpg' },
+            { full: 'product02-image02-full.jpg', thumb: 'product02-image02-thumb.jpg' },
+          ],
           description: "This product is ALSO awesome!",
           specifications: "Filled with Awesome",
           reviews: [ ]
@@ -17,14 +24,14 @@
       ];
     })
     .controller("GalleryController", function(){
-      var current;
+      var current = 0;
 
-      this.setCurrent = function(image){
-        current = image;
+      this.setCurrent = function(index){
+        current = index;
       };
 
-      this.isCurrent = function(image){
-        return image === current;
+      this.getCurrent = function(images){
+        return images[current];
       };
     })
     .controller("PanelController", function(){
@@ -53,6 +60,13 @@
         replace: true,
         restrict: 'E',
         templateUrl: 'product-title.html',
+      };
+    })
+    .directive("productGallery", function(){
+      return {
+        replace: true,
+        restrict: 'E',
+        templateUrl: 'product-gallery.html',
       };
     })
   ; // END module(store)
